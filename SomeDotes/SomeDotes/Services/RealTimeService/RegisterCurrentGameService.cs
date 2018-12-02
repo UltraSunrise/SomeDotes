4a2fb797d7c97f1e2a5896c4ad1cff7b79d36eb8
Iavor Orlyov <iavor_orlyov@yahoo.com>
2018-12-02 19:37:52 +0200

    #13 Added ViewModel and moved the logic

--- SomeDotes/SomeDotes/Services/RealTimeService/RegisterCurrentGameService.cs
+++ SomeDotes/SomeDotes/Services/RealTimeService/RegisterCurrentGameService.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using SomeDotes.Data.Entities;
-using SomeDotes.Models.Intefaces;
-using SomeDotes.Services.DatabaseServices;
-
-namespace SomeDotes.Services.RealTimeService
+﻿namespace SomeDotes.Services.RealTimeService
 {
     using Microsoft.Win32;
     using SomeDotes.Models.Interfaces;
-    using SomeDotes.Models.JSONModels.RealtimeGameModels;
     using SomeDotes.Models.MainModels;
     using SomeDotes.Services.Helpers;
-    using System.Collections.Generic;
+    using SomeDotes.ViewModels;
     using System.Diagnostics;
     using System.IO;
-    using System.Linq;
 
     public class RegisterCurrentGameService : IRegisterCurrentGameService
     {
@@ -21,9 +14,6 @@ namespace SomeDotes.Services.RealTimeService
 
         private ICurrentGameService cgs;
         private IConvertSteamIdToAccountId converter;
-        private IDatabaseService dbService;
-        private bool isNewGame;
-        private readonly string PRIVATE_ACCOUNT = "4294967295";
         private MatchInfo currentMatchInfo;
         private static RegisterCurrentGameService singelton;
 
@@ -33,9 +23,7 @@ namespace SomeDotes.Services.RealTimeService
 
         private RegisterCurrentGameService()
         {
-            dbService = new DatabaseService();
             converter = new ConvertSteamIdToAccountId();
-            isNewGame = true;
         }
 
         #endregion
@@ -83,93 +71,9 @@ namespace SomeDotes.Services.RealTimeService
 
         private void OnNewGameState(CurrentGameStateService gs)
         {
-            RootObject ro = gs.ParsedData;
+            CurrentGameViewModel currentGameViewModel = new CurrentGameViewModel(gs);
 
-            if (gs.ParsedData.Map == null)
-            {
-                return;
-            }
-            
-            var steamIds = FindAllSteamIds(gs.ParsedData);
-            MatchInfo matchInfo = new MatchInfo();
-
-            matchInfo.MatchID = gs.ParsedData.Map.MatchId;
-            matchInfo.RadiantGold = gs.ParsedData.MainPlayer.RadiantTeam.Player0.NetWorth;
-            matchInfo.DireGold = gs.ParsedData.MainPlayer.DireTeam.Player5.NetWorth;
-            matchInfo.WinProbability = gs.ParsedData.Map.RadiantWinChance;
-            matchInfo.RadiantKills = gs.ParsedData.MainPlayer.RadiantTeam.GetAllPlayers.Sum(p => p.Kills);
-            matchInfo.DireKills = gs.ParsedData.MainPlayer.DireTeam.GetAllPlayers.Sum(p => p.Kills);
-            TimeSpan t = TimeSpan.FromSeconds(gs.ParsedData.Map.ClockTime);
-            matchInfo.MatchTime = string.Format("{0:D2}:{1:D2}",
-                                            t.Minutes,
-                                            t.Seconds);
-
-            //All players
-            matchInfo.RadiantTeam = gs.ParsedData.MainPlayer.RadiantTeam.GetAllPlayers;
-            matchInfo.DireTeam = gs.ParsedData.MainPlayer.DireTeam.GetAllPlayers;
-            //All players current networth
-            matchInfo.HeroesNetWorth.AddRange(gs.ParsedData.MainPlayer.RadiantTeam.GetAllPlayers.Select(gap => gap.NetWorth));
-            matchInfo.HeroesNetWorth.AddRange(gs.ParsedData.MainPlayer.DireTeam.GetAllPlayers.Select(gap => gap.NetWorth));
-            //All players current gold
-            matchInfo.HeroesCurrentGold.AddRange(gs.ParsedData.MainPlayer.RadiantTeam.GetAllPlayers.Select(gap => gap.Gold));
-            matchInfo.HeroesCurrentGold.AddRange(gs.ParsedData.MainPlayer.DireTeam.GetAllPlayers.Select(gap => gap.Gold));
-            //All players reliable gold
-            matchInfo.HeroesReliableGold.AddRange(gs.ParsedData.MainPlayer.RadiantTeam.GetAllPlayers.Select(gap => gap.GoldReliable));
-            matchInfo.HeroesReliableGold.AddRange(gs.ParsedData.MainPlayer.DireTeam.GetAllPlayers.Select(gap => gap.GoldReliable));
-            //All players buybackcost
-            matchInfo.HeroesBuybackCost.AddRange(gs.ParsedData.Hero.RadiantTeam.AllPlayers.Select(p => p.BuybackCost));
-            matchInfo.HeroesBuybackCost.AddRange(gs.ParsedData.Hero.DireTeam.AllPlayers.Select(p => p.BuybackCost));
-            //All players buyback cooldown
-            matchInfo.HeroesBuybackCooldown.AddRange(gs.ParsedData.Hero.RadiantTeam.AllPlayers.Select(p => p.BuybackCooldown));
-            matchInfo.HeroesBuybackCooldown.AddRange(gs.ParsedData.Hero.DireTeam.AllPlayers.Select(p => p.BuybackCooldown));
-            
-            LoadImages(matchInfo, gs);
-
-            CurrentMatchInfo = matchInfo;
-
-            isNewGame = false;
-        }
-
-        private List<string> FindAllSteamIds(RootObject parsedData)
-        {
-            List<string> allSteamIds = new List<string>();
-
-            allSteamIds.AddRange(parsedData.MainPlayer.RadiantTeam.GetAllPlayers.Select(p => p.SteamId));
-            allSteamIds.AddRange(parsedData.MainPlayer.DireTeam.GetAllPlayers.Select(p => p.SteamId));
-
-            return allSteamIds.Where(asi => asi != PRIVATE_ACCOUNT).ToList();
-        }
-
-        private HeroDb GetHero(long id)
-        {
-            var currentHero = dbService.GetAllHeroes().FirstOrDefault(h => h.HeroId == id);
-
-            return currentHero;
-        }
-
-        private void LoadImages(MatchInfo matchInfo, CurrentGameStateService gs)
-        {
-            ImagesHelper imagesHelper = ImagesHelper.GetInstance();
-
-            if (!imagesHelper.AreAllAdded)
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    if (imagesHelper.AllImages[i] == null)
-                    {
-                        imagesHelper.AllImages[i] = ConvertByteArrayToString(GetHero(gs.ParsedData.Hero.AllHeroes[i].Id));
-                    }
-                    if (imagesHelper.AllImages.Where(ai => ai != null).Count() == 10)
-                        imagesHelper.AreAllAdded = true;
-                }
-            }
-            matchInfo.HeroesImages = imagesHelper.AllImages;
-        }
-
-        private string ConvertByteArrayToString(HeroDb hero)
-        {
-            return string.Format("data:image/png;base64,{0}",
-                Convert.ToBase64String(hero?.ImageFull));
+            CurrentMatchInfo = currentGameViewModel.CurrentMatchInfo;
         }
 
         #region RegisterGame
---
+++ SomeDotes/SomeDotes/ViewModels/CurrentGameViewModel.cs
@@ -0,0 +1,128 @@
+﻿using SomeDotes.Data.Entities;
+using SomeDotes.Models.MainModels;
+using SomeDotes.Services.Helpers;
+using SomeDotes.Services.RealTimeService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SomeDotes.ViewModels
+{
+    using SomeDotes.Models.Intefaces;
+    using SomeDotes.Models.JSONModels.RealtimeGameModels;
+    using SomeDotes.Services.DatabaseServices;
+
+    public class CurrentGameViewModel
+    {
+        private readonly string PRIVATE_ACCOUNT = "4294967295";
+
+        private CurrentGameStateService gs;
+        private IDatabaseService dbService;
+        private MatchInfo currentMatchInfo;
+
+        public CurrentGameViewModel(CurrentGameStateService gs)
+        {
+            this.gs = gs;
+            dbService = new DatabaseService();
+
+            RootObject ro = gs.ParsedData;
+
+            if (gs.ParsedData.Map == null)
+            {
+                return;
+            }
+
+            var steamIds = FindAllSteamIds(gs.ParsedData);
+            MatchInfo matchInfo = new MatchInfo();
+
+            matchInfo.MatchID = gs.ParsedData.Map.MatchId;
+            matchInfo.RadiantGold = gs.ParsedData.MainPlayer.RadiantTeam.Player0.NetWorth;
+            matchInfo.DireGold = gs.ParsedData.MainPlayer.DireTeam.Player5.NetWorth;
+            matchInfo.WinProbability = gs.ParsedData.Map.RadiantWinChance;
+            matchInfo.RadiantKills = gs.ParsedData.MainPlayer.RadiantTeam.GetAllPlayers.Sum(p => p.Kills);
+            matchInfo.DireKills = gs.ParsedData.MainPlayer.DireTeam.GetAllPlayers.Sum(p => p.Kills);
+            TimeSpan t = TimeSpan.FromSeconds(gs.ParsedData.Map.ClockTime);
+            matchInfo.MatchTime = string.Format("{0:D2}:{1:D2}",
+                                            t.Minutes,
+                                            t.Seconds);
+
+            //All players
+            matchInfo.RadiantTeam = gs.ParsedData.MainPlayer.RadiantTeam.GetAllPlayers;
+            matchInfo.DireTeam = gs.ParsedData.MainPlayer.DireTeam.GetAllPlayers;
+            //All players current networth
+            matchInfo.HeroesNetWorth.AddRange(gs.ParsedData.MainPlayer.RadiantTeam.GetAllPlayers.Select(gap => gap.NetWorth));
+            matchInfo.HeroesNetWorth.AddRange(gs.ParsedData.MainPlayer.DireTeam.GetAllPlayers.Select(gap => gap.NetWorth));
+            //All players current gold
+            matchInfo.HeroesCurrentGold.AddRange(gs.ParsedData.MainPlayer.RadiantTeam.GetAllPlayers.Select(gap => gap.Gold));
+            matchInfo.HeroesCurrentGold.AddRange(gs.ParsedData.MainPlayer.DireTeam.GetAllPlayers.Select(gap => gap.Gold));
+            //All players reliable gold
+            matchInfo.HeroesReliableGold.AddRange(gs.ParsedData.MainPlayer.RadiantTeam.GetAllPlayers.Select(gap => gap.GoldReliable));
+            matchInfo.HeroesReliableGold.AddRange(gs.ParsedData.MainPlayer.DireTeam.GetAllPlayers.Select(gap => gap.GoldReliable));
+            //All players buybackcost
+            matchInfo.HeroesBuybackCost.AddRange(gs.ParsedData.Hero.RadiantTeam.AllPlayers.Select(p => p.BuybackCost));
+            matchInfo.HeroesBuybackCost.AddRange(gs.ParsedData.Hero.DireTeam.AllPlayers.Select(p => p.BuybackCost));
+            //All players buyback cooldown
+            matchInfo.HeroesBuybackCooldown.AddRange(gs.ParsedData.Hero.RadiantTeam.AllPlayers.Select(p => p.BuybackCooldown));
+            matchInfo.HeroesBuybackCooldown.AddRange(gs.ParsedData.Hero.DireTeam.AllPlayers.Select(p => p.BuybackCooldown));
+
+            LoadImages(matchInfo, gs);
+
+            CurrentMatchInfo = matchInfo;
+        }
+
+        public MatchInfo CurrentMatchInfo
+        {
+            get
+            {
+                return currentMatchInfo;
+            }
+            set
+            {
+                currentMatchInfo = value;
+            }
+        }
+
+
+        private List<string> FindAllSteamIds(RootObject parsedData)
+        {
+            List<string> allSteamIds = new List<string>();
+
+            allSteamIds.AddRange(parsedData.MainPlayer.RadiantTeam.GetAllPlayers.Select(p => p.SteamId));
+            allSteamIds.AddRange(parsedData.MainPlayer.DireTeam.GetAllPlayers.Select(p => p.SteamId));
+
+            return allSteamIds.Where(asi => asi != PRIVATE_ACCOUNT).ToList();
+        }
+
+        private HeroDb GetHero(long id)
+        {
+            var currentHero = dbService.GetAllHeroes().FirstOrDefault(h => h.HeroId == id);
+
+            return currentHero;
+        }
+
+        private void LoadImages(MatchInfo matchInfo, CurrentGameStateService gs)
+        {
+            ImagesHelper imagesHelper = ImagesHelper.GetInstance();
+
+            if (!imagesHelper.AreAllAdded)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (imagesHelper.AllImages[i] == null)
+                    {
+                        imagesHelper.AllImages[i] = ConvertByteArrayToString(GetHero(gs.ParsedData.Hero.AllHeroes[i].Id));
+                    }
+                    if (imagesHelper.AllImages.Where(ai => ai != null).Count() == 10)
+                        imagesHelper.AreAllAdded = true;
+                }
+            }
+            matchInfo.HeroesImages = imagesHelper.AllImages;
+        }
+
+        private string ConvertByteArrayToString(HeroDb hero)
+        {
+            return string.Format("data:image/png;base64,{0}",
+                Convert.ToBase64String(hero?.ImageFull));
+        }
+    }
+}
