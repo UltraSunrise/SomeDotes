c0cb57f377961b74c5f9a8d3eb6f70d86c0a825e
Iavor Orlyov <iavor_orlyov@yahoo.com>
2018-12-04 12:20:57 +0200

    #17 Added ViewModel

---
+++ SomeDotes/SomeDotes/ViewModels/PreGameViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace SomeDotes.ViewModels
+{
+    using SomeDotes.Helpers;
+    using SomeDotes.Models.Intefaces;
+    using SomeDotes.Services.DatabaseServices;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PreGameViewModel
+    {
+        IDatabaseService dbService;
+        public PreGameViewModel()
+        {
+            dbService = new DatabaseService();
+            LoadData();
+        }
+
+        public List<HeroWinPercentageHelper> MostWinningHeroes { get; set; }
+        public List<HeroWinPercentageHelper> MostLosingHeroes { get; set; }
+
+        private void LoadData()
+        {
+            dbService = new DatabaseService();
+            var heroesWinChance = dbService.GetAllHeroesWinChance();
+
+            MostWinningHeroes = heroesWinChance.OrderByDescending(wc => wc.WinChance).Take(5).ToList();
+            MostLosingHeroes = heroesWinChance.OrderBy(wc => wc.WinChance).Take(5).ToList();
+        }
+    }
+}
