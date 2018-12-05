37b1d9b87fa2796ff35f2074d8788d4594e0ad79
Iavor Orlyov <iavor_orlyov@yahoo.com>
2018-12-05 12:44:26 +0200

    #19 Added new Converter

---
+++ SomeDotes/SomeDotes/Converters/SteamIdConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace SomeDotes.Converters
+{
+    public static class SteamIdConverter
+    {
+        public static long ConvertSteamIdToAccountId(long steamId)
+        {
+            return steamId - 76561197960265728;
+        }
+    }
+}
